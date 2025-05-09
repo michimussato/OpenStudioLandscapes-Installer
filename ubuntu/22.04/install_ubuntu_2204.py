845ad6995710036129f37f96e16a7383e7198ee5
Michael Mussato <michimussato@gmail.com>
2025-05-09 12:29:37 +0200

    refactor

--- ubuntu/22.04/install_ubuntu_2204.py
+++ ubuntu/22.04/install_ubuntu_2204.py
@@ -877,6 +877,7 @@ if __name__ == "__main__":
     OPENSTUDIOLANDSCAPES_DIR = pathlib.Path(input() or OPENSTUDIOLANDSCAPES_DIR).expanduser()
 
     print(f"Install Directory is: {OPENSTUDIOLANDSCAPES_DIR.as_posix()}")
+    print("".center(_get_terminal_size()[0], "#"))
 
     ret_script_disable_unattended_upgrades = script_run(
         sudo=True,
